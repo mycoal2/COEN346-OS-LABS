479fde827a92424e9003ea38cc77d4459a39ef0c
mycoal2 <michaelhong136@gmail.com>
2023-02-06 23:12:34 -0500

    added comments and changed main class to driver

--- ca/concordia/processmanagement/Operation.java
+++ ca/concordia/processmanagement/Operation.java
@@ -3,6 +3,6 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public interface Operation {
-    int createProcess() throws Exception;
+    int createProcess() throws Exception;               
     void terminateProcess(int pid) throws Exception;
 }
