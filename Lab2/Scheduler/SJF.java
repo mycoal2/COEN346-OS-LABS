5b2405d7b52f79ff4a2f5156355b39c81fffd33b
Rihazul Islam <99052898+Rihazul@users.noreply.github.com>
2023-03-05 12:05:04 -0500

    Update SJF.java
    SJF done

--- Lab2/Scheduler/SJF.java
+++ Lab2/Scheduler/SJF.java
@@ -1,70 +1,90 @@
 package Scheduler;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.*;
+import static java.lang.Thread.sleep;
 
 public class SJF implements Algorithm {
-    Queue<Task> queue = new LinkedList<>();
-    Queue<Task> waiting_queue=new LinkedList<>();   
 
-    public void schedule() {
-      int time=0;
-      Clock timer=new Clock();
-      timer.start();
+    List<Task> queue = new ArrayList<Task>();
+    List<Task> readyQueue = new ArrayList<Task>();
+    Clock clock1 = new Clock();
 
-      while(queue.size()>0)
-      {
-         Task temp=queue.remove();
-
-         if(timer.clock<=temp.getPriority())
-         {
-            try{
-               Thread.sleep((temp.getPriority()-timer.clock)*1000))
-            }catch (Exception e) {
-                    System.out.println("error1");
-                }
-         }
-         System.out.println(temp.getName() + " has started");
-         
-         Task check=queue.peek();
-         if(check.getPriority()==timer.clock)
-         {
-            check=queue.remove();
-            manage_queue(check);
-         }
-
-         try{
-             sleep(temp.getBurst() * 1000);
-            } catch (Exception e) {
-               System.out.println("error");
-            }
-            System.out.println("This task has finished");
-
-      }
-         
-      }
-
-      public void manage_queue(Task another_task)
-      {
-         if(waiting_queue.size()<=0)
-         {
-            waiting_queue.add(another_task);
-         }
-
-         else
-         {
-            Task temp=waiting_queue.remove();
-            if(another_task.getBurst()<temp.getBurst())
-            {
-               waiting_queue.add(another_task);
-               waiting_queue.add(temp);
-            }
-         }
-
-      }
-
-        
+    public SJF(List<Task> q) {
+        queue = q;
     }
-     
-     public Task pickNextTask() {
-        Task a = new Task("name",5,2);
-        return a;
-     }
+
+    @Override
+    public void schedule() {
+        clock1.start();
+
+        while (queue.size() > 0 || readyQueue.size() > 0) {
+            Task temp = pickNextTask();
+            System.out.println(temp.getName() + " has started");
+            try {
+                sleep(temp.getBurst() * 1000 + 1);
+            } catch (Exception e) {
+                System.out.println("error burst");
+            }
+            System.out.println(temp.getName() + " has finished");
+        }
+        System.out.println("All Tasks are finished");
+        clock1.stopClock();
+    }
+
+    @Override
+    public Task pickNextTask() {
+        //sort the queue with arrival time
+        Task[] new_arr = queue.toArray(new Task[0]);
+
+        for (int i = 0; i < new_arr.length; i++) {
+            // Inner nested loop pointing 1 index ahead
+            for (int j = i + 1; j < new_arr.length; j++) {
+                // Checking elements
+                Task temp = null;
+                if (new_arr[j].getArrivalTime() < new_arr[i].getArrivalTime()) {
+
+                    // Swapping
+                    temp = new_arr[i];
+                    new_arr[i] = new_arr[j];
+                    new_arr[j] = temp;
+                }
+            }
+        }
+        queue = new ArrayList<Task>(Arrays.asList(new_arr));
+
+        //release each task with clock
+        int i=0;
+        int queue_length= queue.size();
+        int previous_index=0;
+        int next_index=1;
+        while(queue.size()>0)
+        {
+            if(queue.get(i).getArrivalTime()<= clock1.clock && queue.get(i).getArrivalTime()==0)
+            {
+                return queue.remove(i);
+
+            }
+            else {
+                readyQueue.add(queue.remove(i));
+                if(readyQueue.size()>1) {
+                    while (previous_index < readyQueue.size() && next_index < readyQueue.size()) {
+                        if(readyQueue.get(previous_index).getBurst()>readyQueue.get(next_index).getBurst())
+                        {
+                            return readyQueue.remove(next_index);
+                        }
+                    }
+                }
+            }
+
+            i++;
+
+        }
+            return null;
+    }
+
+
 }
