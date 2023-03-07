1c178c8d793db162bea7e2ce08157e73962b5c51
mycoal2 <michaelhong136@gmail.com>
2023-03-06 19:53:34 -0500

    .

--- Lab2/Scheduler/PriorityRR.java
+++ Lab2/Scheduler/PriorityRR.java
@@ -19,56 +19,12 @@ public class PriorityRR implements Algorithm {
 
    @Override
    public void schedule() {
-       Clock clock1 = new Clock();
-       clock1.start();
-       while(queue.size() > 0) {
-           Task temp = queue.remove(0);
-
-           if(clock1.clock <= temp.getArrivalTime()) {
-               try {
-                   Thread.sleep((temp.getArrivalTime() - clock1.clock) * 1000);
-               } catch (Exception e) {
-                   System.out.println("error1");
-               }
-           }
-           System.out.println("This task has started: " + temp);
-           try {
-               sleep(temp.getBurst() * 1000 );
-           } catch (Exception e) {
-               System.out.println("error burst");
-           }
-
-           System.out.println("Task " + temp.getName() + " has finished");
-
-       }
-       clock1.stopClock();
-       System.out.println("All Tasks are finished");
+     
    }
 
    @Override
    public Task pickNextTask() {
-       Task[] arr = queue.toArray(new Task[0]);
-       
-       for (int i = 0; i < arr.length; i++) {
-           // Inner nested loop pointing 1 index ahead
-           for (int j = i + 1; j < arr.length; j++) {
-               // Checking elements
-               Task temp = null;
-               if (arr[j].getArrivalTime() < arr[i].getArrivalTime() ) {
-
-                   // Swapping
-                   temp = arr[i];
-                   arr[i] = arr[j];
-                   arr[j] = temp;
-               }
-           }
-           // Printing sorted array elements
-           //System.out.println(arr[i] + " ");
-       }
-
-       queue = new ArrayList<Task>(Arrays.asList(arr));            //put sorted array to queue.
-
-       return null;
+      return null;
    }
 
 }
